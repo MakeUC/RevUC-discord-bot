a17237baa64111a211d963652eac609453e0ed12
Dev Agrawal <agrawadv@mail.uc.edu>
2021-06-06 07:41:31 -0400

    arguments binding fixed

--- discord/background.js
+++ discord/background.js
@@ -1,6 +1,7 @@
-function background(arguments, receivedMessage){
-  const intro = "Oh you wanna know about my background... okay, here goes.\nI was conceived by one Super User Dev Olpowerful (SUDO), and I was brought to life by one Piyush Rap!d Sahu (PRS)."
+function background(commandArguments, receivedMessage) {
+  const intro =
+    'Oh you wanna know about my background... okay, here goes.\nI was conceived by one Super User Dev Olpowerful (SUDO), and I was brought to life by one Piyush Rap!d Sahu (PRS).'
   receivedMessage.channel.send(intro)
 }
 
-module.exports=background
+module.exports = background
--- discord/index.js
+++ discord/index.js
@@ -1,89 +1,88 @@
-require('dotenv').config();
-const client = require("./client");
-const help = require("./help")
+require('dotenv').config()
+const client = require('./client')
+const help = require('./help')
 const checkin = require('./checkin')
 const { score, top } = require('./score')
-const revvit = require("./revvit")
-const background = require("./background")
-const addAttendee = require("./add-attendee")
+const revvit = require('./revvit')
+const background = require('./background')
+const addAttendee = require('./add-attendee')
 
 const CHECKIN_CHANNEL_ID = process.env.CHECKIN_CHANNEL_ID
 
 client.once('ready', () => {
-    console.log('the bot is online!')
-    client.user.setActivity('with Javascript', {type: "PLAYING"})
+  console.log('the bot is online!')
+  client.user.setActivity('with Javascript', { type: 'PLAYING' })
 
-    // list all servers the bot is connected to
-    console.log('\nServers:')
-    client.guilds.cache.forEach((guild) => {
-        console.log(` - ${guild.name}`)
+  // list all servers the bot is connected to
+  console.log('\nServers:')
+  client.guilds.cache.forEach((guild) => {
+    console.log(` - ${guild.name}`)
 
-        // list all channels in the server
-        guild.channels.cache.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-        })
+    // list all channels in the server
+    guild.channels.cache.forEach((channel) => {
+      console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
     })
+  })
 })
 
 client.on('message', async (receivedMessage) => {
-    if(receivedMessage.author == client.user) {
-        return
-    }
+  if (receivedMessage.author == client.user) {
+    return
+  }
 
-    if(receivedMessage.channel.id == CHECKIN_CHANNEL_ID) {
-        // run the checkin function
-        await checkin(receivedMessage)
-        return;
-    }
+  if (receivedMessage.channel.id == CHECKIN_CHANNEL_ID) {
+    // run the checkin function
+    await checkin(receivedMessage)
+    return
+  }
 
-    if(receivedMessage.content.startsWith('!')) {
-        processCommand(receivedMessage)
-    }
+  if (receivedMessage.content.startsWith('!')) {
+    processCommand(receivedMessage)
+  }
 })
 
 /**
- * 
- * @param {Discord.Message} receivedMessage 
+ *
+ * @param {Discord.Message} receivedMessage
  */
 function processCommand(receivedMessage) {
-    // remove the leading/bot identifying character
-    const fullCommand = receivedMessage.content.substr(1)
-    
-    // split the message up into pieces for each space
-    const splitCommand = fullCommand.split(" ")
-    
-    // take the first string from the split
-    const primaryCommand = splitCommand[0]
-    
-    // all other words are arguments/parameters for the primaryCommand
-    const arguments = splitCommand.slice(1)
+  // remove the leading/bot identifying character
+  const fullCommand = receivedMessage.content.substr(1)
 
-    console.log('command received: ' + primaryCommand)
-    console.log('Arguments: ' + arguments)
+  // split the message up into pieces for each space
+  const splitCommand = fullCommand.split(' ')
 
-    switch(primaryCommand) {
-        case 'help':
-            help(arguments, receivedMessage)
-            break
-        case 'claim':
-            score(arguments, receivedMessage)
-            break
-        case 'top':
-            top(arguments, receivedMessage)
-            break
-        case 'hi':
-        case 'revvit':
-            revvit(arguments, receivedMessage)
-            break
-        case 'background':
-            background(arguments, receivedMessage)
-            break
-        case 'sponsor':
-        case 'mentor':
-        case 'judge':
-        case 'minor':
-            addAttendee(arguments, receivedMessage, primaryCommand)
-            break;
-    }
-    
+  // take the first string from the split
+  const primaryCommand = splitCommand[0]
+
+  // all other words are arguments/parameters for the primaryCommand
+  const commandArguments = splitCommand.slice(1)
+
+  console.log('command received: ' + primaryCommand)
+  console.log('Arguments: ' + commandArguments)
+
+  switch (primaryCommand) {
+    case 'help':
+      help(commandArguments, receivedMessage)
+      break
+    case 'claim':
+      score(commandArguments, receivedMessage)
+      break
+    case 'top':
+      top(commandArguments, receivedMessage)
+      break
+    case 'hi':
+    case 'revvit':
+      revvit(commandArguments, receivedMessage)
+      break
+    case 'background':
+      background(commandArguments, receivedMessage)
+      break
+    case 'sponsor':
+    case 'mentor':
+    case 'judge':
+    case 'minor':
+      addAttendee(commandArguments, receivedMessage, primaryCommand)
+      break
+  }
 }
