d9f8f45f28e900f13c67095fb5ffe8fe9fb8a221
Dev Agrawal <devagrawal09@gmail.com>
2021-06-13 21:49:23 -0400

    updated auth to always throw 401

--- server/auth.js
+++ server/auth.js
@@ -8,11 +8,15 @@ const SERVER_KEY = process.env.SERVER_KEY
  * @param {express.NextFunction} next
  */
 const auth = (req, res, next) => {
-  const token = req.headers.authorization.split(' ')[1]
-  if (token !== SERVER_KEY) {
-    res.sendStatus(401)
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    if (token !== SERVER_KEY) {
+      throw 401;
+    }
+    next()
+  } catch (err) {
+    res.sendStatus(401);
   }
-  next()
 }
 
 module.exports = auth
